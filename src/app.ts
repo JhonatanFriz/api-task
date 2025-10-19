1d544c8ccbada01a81fd2bef85240f504ae9ce2d
Jhonatan Friz <jhonatan.friz@alumnos.uach.cl>
2025-10-19 01:32:00 -0300

    se agrega cors para comunicación con cliente

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 ﻿import express from 'express';
+import cors from 'cors';
 import { tasksRouter } from './routes/tasks.js';
 
 const app = express();
+app.use(cors({ origin :['http://localhost:5173',
+    'https://task-client.vercel.app'] }));
 app.use(express.json());
 
 app.get('/health', (_req, res) => res.json({ ok: true, service: 'API-REST TAREAS' }));
